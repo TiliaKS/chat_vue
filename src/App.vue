8c6e960b63f620cf609323ce06356f860824a9b8
Serhii Kotelevets <serhii_kotelevets@epam.com>
2020-05-18 21:59:01 +0300

    Login page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Notifier />
     <router-view/>
   </div>
 </template>
 
+<script>
+import Notifier from '@/components/Notification.vue';
+
+export default {
+  components: {
+    Notifier,
+  },
+};
+</script>
+
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/plugins/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAwGGw7fzfs1RB6ofKsATXsxSKfNnZw4Uc',
+  authDomain: 'chatapp-fea6f.firebaseapp.com',
+  databaseURL: 'https://chatapp-fea6f.firebaseio.com',
+  projectId: 'chatapp-fea6f',
+  storageBucket: 'chatapp-fea6f.appspot.com',
+  messagingSenderId: '216130948696',
+  appId: '1:216130948696:web:541b5777470b204225c42d',
+};
+
+export default firebaseConfig;
